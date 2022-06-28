9e3c0382a9588c26d5cffbadfe2ce971138180b9
Sisir Potluri <sisir@umich.edu>
2022-06-27 21:11:11 -0400

    remove hardcoded API credentials

--- utils/fetchTools.js
+++ utils/fetchTools.js
@@ -1,6 +1,7 @@
 export const baseUrl = 'https://free-news.p.rapidapi.com/v1'
 
 export const credentials = {
-    'X-RapidAPI-Key': 'b64f390458msh87a897f50fac2d6p1257e5jsn17f6dbfc4396',
+    'X-RapidAPI-Key': '******', /* Replace this with your 'X-RapidAPI-Key' that you can obtain with a free subscription to the 
+                                   https://free-news.p.rapidapi.com API on RapidAPI */
     'X-RapidAPI-Host': 'free-news.p.rapidapi.com'
 }
